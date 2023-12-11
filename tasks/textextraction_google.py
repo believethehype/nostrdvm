a03efeab0977be88090f374c397cec44a779d26d
Believethehype <tobias_baur@yahoo.com>
2023-12-11 19:41:00 +0100

    first version of llm dvm (with replaceable backend)

--- tasks/textextraction_google.py
+++ tasks/textextraction_google.py
@@ -178,7 +178,6 @@ if __name__ == '__main__':
     admin_config = AdminConfig()
     admin_config.REBROADCAST_NIP89 = False
     admin_config.UPDATE_PROFILE = False
-    admin_config.LUD16 = ""
     dvm = build_example("Transcriptor", "speech_recognition", admin_config)
     dvm.run()
 
--- tasks/textgeneration_llmlite.py
+++ tasks/textgeneration_llmlite.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import dotenv
 import requests
-from PIL import Image
+from litellm import completion
 
 from interfaces.dvmtaskinterface import DVMTaskInterface
 from utils.admin_utils import AdminConfig
@@ -26,14 +26,16 @@ Outputs: An url to an Image
 """
 
 
-class ImageGenerationDALLE(DVMTaskInterface):
-    KIND: int = EventDefinitions.KIND_NIP90_GENERATE_IMAGE
-    TASK: str = "text-to-image"
-    FIX_COST: float = 120
+class TextGenerationOLLAMA(DVMTaskInterface):
+    KIND: int = EventDefinitions.KIND_NIP90_GENERATE_TEXT
+    TASK: str = "text-to-text"
+    FIX_COST: float = 0
 
     def __init__(self, name, dvm_config: DVMConfig, nip89config: NIP89Config,
                  admin_config: AdminConfig = None, options=None):
         super().__init__(name, dvm_config, nip89config, admin_config, options)
+        if options is not None and options.get("server"):
+            self.options["server"] = options["server"]
 
     def is_input_supported(self, tags):
         for tag in tags:
@@ -43,23 +45,14 @@ class ImageGenerationDALLE(DVMTaskInterface):
                 if input_type != "text":
                     return False
 
-            elif tag.as_vec()[0] == 'output':
-                output = tag.as_vec()[1]
-                if (output == "" or
-                        not (output == "image/png" or "image/jpg"
-                             or output == "image/png;format=url" or output == "image/jpg;format=url")):
-                    print("Output format not supported, skipping..")
-                    return False
-
         return True
 
     def create_request_from_nostr_event(self, event, client=None, dvm_config=None):
         request_form = {"jobID": event.id().to_hex() + "_" + self.NAME.replace(" ", "")}
         prompt = ""
-        width = "1024"
-        height = "1024"
-        model = "dall-e-3"
-        quality = "standard"
+        model = "ollama/llama2-uncensored" #ollama/nous-hermes # This requires an instance of OLLAMA running
+        #model = "gpt-4-1106-preview" # This will call chatgpt and requires an OpenAI API Key set in .env
+        server = "http://localhost:11434"
 
         for tag in event.tags():
             if tag.as_vec()[0] == 'i':
@@ -67,58 +60,39 @@ class ImageGenerationDALLE(DVMTaskInterface):
                 if input_type == "text":
                     prompt = tag.as_vec()[1]
 
-            elif tag.as_vec()[0] == 'param':
-                print("Param: " + tag.as_vec()[1] + ": " + tag.as_vec()[2])
-                if tag.as_vec()[1] == "size":
-                    if len(tag.as_vec()) > 3:
-                        width = (tag.as_vec()[2])
-                        height = (tag.as_vec()[3])
-                    elif len(tag.as_vec()) == 3:
-                        split = tag.as_vec()[2].split("x")
-                        if len(split) > 1:
-                            width = split[0]
-                            height = split[1]
-                elif tag.as_vec()[1] == "quality":
-                    quality = tag.as_vec()[2]
-
         options = {
             "prompt": prompt,
-            "size": width + "x" + height,
             "model": model,
-            "quality": quality,
-            "number": 1
+            "server": server
         }
         request_form['options'] = json.dumps(options)
 
         return request_form
 
     def process(self, request_form):
+        options = DVMTaskInterface.set_options(request_form)
+
         try:
-            options = DVMTaskInterface.set_options(request_form)
-
-            from openai import OpenAI
-            client = OpenAI()
-            print("Job " + request_form['jobID'] + " sent to OpenAI API..")
-
-            response = client.images.generate(
-                model=options['model'],
-                prompt=options['prompt'],
-                size=options['size'],
-                quality=options['quality'],
-                n=int(options['number']),
-            )
-
-            image_url = response.data[0].url
-            # rehost the result instead of relying on the openai link
-            response = requests.get(image_url)
-            image = Image.open(BytesIO(response.content)).convert("RGB")
-            image.save("./outputs/image.jpg")
-            result = upload_media_to_hoster("./outputs/image.jpg")
-            return result
+            if options["model"].startswith("ollama"):
+                response = completion(
+                    model=options["model"],
+                    messages=[{"content": options["prompt"], "role": "user"}],
+                    api_base=options["server"]
+                )
+                print(response.choices[0].message.content)
+                return response.choices[0].message.content
+            else:
+                response = completion(
+                    model=options["model"],
+                    messages=[{"content": options["prompt"], "role": "user"}],
+                )
+                print(response.choices[0].message.content)
+                return response.choices[0].message.content
 
         except Exception as e:
-            print("Error in Module")
+            print("Error in Module: " + str(e))
             raise Exception(e)
+
 
 # We build an example here that we can call by either calling this file directly from the main directory,
 # or by adding it to our playground. You can call the example and adjust it to your needs or redefine it in the
@@ -132,8 +106,6 @@ def build_example(name, identifier, admin_config):
     dvm_config.LNBITS_ADMIN_KEY = admin_key  # The dvm might pay failed jobs back
     dvm_config.LNBITS_URL = os.getenv("LNBITS_HOST")
     admin_config.LUD16 = lnaddress
-    profit_in_sats = 10
-    dvm_config.FIX_COST = int(((4.0 / (get_price_per_sat("USD") * 100)) + profit_in_sats))
 
     nip90params = {
         "size": {
@@ -144,19 +116,18 @@ def build_example(name, identifier, admin_config):
     nip89info = {
         "name": name,
         "image": "https://image.nostr.build/c33ca6fc4cc038ca4adb46fdfdfda34951656f87ee364ef59095bae1495ce669.jpg",
-        "about": "I use OpenAI's DALL·E 3",
+        "about": "I use a LLM connected via OLLAMA",
         "encryptionSupported": True,
         "cashuAccepted": True,
         "nip90Params": nip90params
     }
 
-
     nip89config = NIP89Config()
     nip89config.DTAG = check_and_set_d_tag(identifier, name, dvm_config.PRIVATE_KEY,
-                                                              nip89info["image"])
+                                           nip89info["image"])
     nip89config.CONTENT = json.dumps(nip89info)
     # We add an optional AdminConfig for this one, and tell the dvm to rebroadcast its NIP89
-    return ImageGenerationDALLE(name=name, dvm_config=dvm_config, nip89config=nip89config, admin_config=admin_config)
+    return TextGenerationOLLAMA(name=name, dvm_config=dvm_config, nip89config=nip89config, admin_config=admin_config)
 
 
 if __name__ == '__main__':
@@ -170,7 +141,8 @@ if __name__ == '__main__':
     admin_config = AdminConfig()
     admin_config.REBROADCAST_NIP89 = False
     admin_config.UPDATE_PROFILE = False
-    dvm = build_example("Dall-E 3", "dalle3", admin_config)
+
+    dvm = build_example("LLM", "llmlite", admin_config)
     dvm.run()
 
     keep_alive()
