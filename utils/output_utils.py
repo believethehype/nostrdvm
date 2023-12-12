8de086da7e6c468228def349ea637585b954ab30
Believethehype <tobias_baur@yahoo.com>
2023-12-12 10:05:25 +0100

    Update output_utils.py

--- utils/output_utils.py
+++ utils/output_utils.py
@@ -175,7 +175,7 @@ def upload_media_to_hoster(filepath: str):
                 print(result)
                 return result
             except:
-                raise Exception("Upload not possible, all hosters didn't work")
+                raise Exception("Upload not possible, all hosters didn't work or couldn't generate output")
 
 def build_status_reaction(status, task, amount, content):
     alt_description = "This is a reaction to a NIP90 DVM AI task. "
