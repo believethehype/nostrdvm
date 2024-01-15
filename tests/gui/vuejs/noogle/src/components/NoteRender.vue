c5668e5d44782b8688dd929b1483627ca3074a0f
Believethehype <tobias_baur@yahoo.com>
2024-01-15 21:09:43 +0100

    cleanup

--- tests/gui/vuejs/noogle/src/components/NoteRender.vue
+++ tests/gui/vuejs/noogle/src/components/NoteRender.vue
@@ -12,54 +12,9 @@ const props = defineProps({
    author: {
     type: String,
     required: true
-  },
-   avatar: {
-    type: String,
-    required: true
-  },
-   authorurl: {
-    type: String,
-    required: true
-  },
+  }
 })
 
-
-async function getEvent(eventid){
-  const event_filter = new Filter().id(EventId.fromHex(eventid)).limit(1)
-  let  client = store.state.client
-  let evts = await client.getEventsOf([event_filter], 5)
-  return evts[0]
-}
-
-
-async function parseContent(pubkey) {
-  let name = PublicKey.fromHex(pubkey).toBech32()
-  let nip05 = ""
-  let lud16 = ""
-  let picture = ""
-  const profile_filter = new Filter().kind(0).author(PublicKey.fromHex(pubkey)).limit(1)
-  let  client = store.state.client
-  let evts = await client.getEventsOf([profile_filter], 5)
-  console.log("PROFILES:" + evts.length)
-  if (evts.length > 0) {
-    let latest_entry = evts[0]
-    let latest_time = 0
-
-    for (const entry of evts) {
-      if (entry.createdAt.asSecs() > latest_time) {
-        latest_time = entry.createdAt.asSecs();
-        latest_entry = entry
-      }
-    }
-
-    let profile = JSON.parse(latest_entry.content);
-    name = profile["name"]
-    picture = profile["picture"]
-    console.log(picture)
-    return name
-
-  }
-}
 
 async function get_user_info(pubkey){
         let client = store.state.client
@@ -85,6 +40,7 @@ async function get_user_info(pubkey){
 const content_placeholder = ref()
 const author_placeholder = ref()
 const author_image_placeholder = ref()
+const author_url_placeholder = ref()
 
 onMounted(async () => {
 
@@ -92,6 +48,7 @@ onMounted(async () => {
    content_placeholder.value = "Event not found"
    author_placeholder.value = ""
    author_image_placeholder.value = ""
+   author_url_placeholder.value = ""
 
 
       content_placeholder.value = props.content //TODO furher parse content
@@ -100,6 +57,7 @@ onMounted(async () => {
       console.log(profile)
       author_placeholder.value = profile["name"]
       author_image_placeholder.value = profile["picture"]
+      author_url_placeholder.value = "https://njump.me/" + PublicKey.fromHex(props.author).toBech32()
 
 })
 
@@ -109,7 +67,7 @@ onMounted(async () => {
 
 <template><div class="playeauthor-wrapper">
         <img class="avatar" :src="author_image_placeholder" alt="" />
-         <a :href="author_placeholder">{{ author_placeholder}}</a>  </div>
+         <a :href="author_url_placeholder" target="_blank">{{ author_placeholder}}</a>  </div>
  <p>{{ content_placeholder }}</p>
 
 
