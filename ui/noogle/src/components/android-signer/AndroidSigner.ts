b7e11c877058a026910b755f3454b2764e11e563
greenart7c3 <greenart7c3@proton.me>
2024-01-31 14:41:15 -0300

    remove some unused code

--- ui/noogle/src/components/android-signer/AndroidSigner.ts
+++ ui/noogle/src/components/android-signer/AndroidSigner.ts
@@ -1,7 +1,9 @@
-import { getEventHash, nip19, verifySignature } from "nostr-tools";
+// taken from https://github.com/hzrd149/nostrudel
+
+import { nip19, verifySignature } from "nostr-tools";
 import createDefer, { Deferred } from "./classes/deffered";
-import { getPubkeyFromDecodeResult, isHex, isHexKey } from "./helpers/nip19";
-import { DraftNostrEvent, NostrEvent } from "./types/nostr-event";
+import { getPubkeyFromDecodeResult, isHexKey } from "./helpers/nip19";
+import { NostrEvent } from "./types/nostr-event";
 
 export function createGetPublicKeyIntent() {
   return `intent:#Intent;scheme=nostrsigner;S.compressionType=none;S.returnType=signature;S.type=get_public_key;end`;
@@ -10,16 +12,6 @@ export function createSignEventIntent(draft) {
   return `intent:${encodeURIComponent(
     JSON.stringify(draft),
   )}#Intent;scheme=nostrsigner;S.compressionType=none;S.returnType=event;S.type=sign_event;end`;
-}
-export function createNip04EncryptIntent(pubkey: string, plainText: string) {
-  return `intent:${encodeURIComponent(
-    plainText,
-  )}#Intent;scheme=nostrsigner;S.pubKey=${pubkey};S.compressionType=none;S.returnType=signature;S.type=nip04_encrypt;end`;
-}
-export function createNip04DecryptIntent(pubkey: string, data: string) {
-  return `intent:${encodeURIComponent(
-    data,
-  )}#Intent;scheme=nostrsigner;S.pubKey=${pubkey};S.compressionType=none;S.returnType=signature;S.type=nip04_decrypt;end`;
 }
 
 let pendingRequest: Deferred<string> | null = null;
@@ -79,26 +71,10 @@ async function signEvent(draft): Promise<NostrEvent> {
   return signedEvent;
 }
 
-async function nip04Encrypt(pubkey: string, plaintext: string): Promise<string> {
-  const data = await intentRequest(createNip04EncryptIntent(pubkey, plaintext));
-  return data;
-}
-async function nip04Decrypt(pubkey: string, data: string): Promise<string> {
-  const plaintext = await intentRequest(createNip04DecryptIntent(pubkey, data));
-  return plaintext;
-}
-
 const amberSignerService = {
   supported: navigator.userAgent.includes("Android") && navigator.clipboard,
   getPublicKey,
-  signEvent,
-  nip04Encrypt,
-  nip04Decrypt,
+  signEvent
 };
-
-// if (import.meta.env.DEV) {
-//   // @ts-ignore
-//   window.amberSignerService = amberSignerService;
-// }
 
 export default amberSignerService;
