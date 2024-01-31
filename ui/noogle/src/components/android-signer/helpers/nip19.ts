c6deaadaab2ec58707f25942f3d3bffa6247c1df
greenart7c3 <greenart7c3@proton.me>
2024-01-31 08:57:21 -0300

    Initial support for amber

---
+++ ui/noogle/src/components/android-signer/helpers/nip19.ts
@@ -0,0 +1,23 @@
+import { getPublicKey, nip19 } from "nostr-tools";
+
+export function isHex(str?: string) {
+  if (str?.match(/^[0-9a-f]+$/i)) return true;
+  return false;
+}
+export function isHexKey(key?: string) {
+  if (key?.toLowerCase()?.match(/^[0-9a-f]{64}$/)) return true;
+  return false;
+}
+
+export function getPubkeyFromDecodeResult(result?: nip19.DecodeResult) {
+  if (!result) return;
+  switch (result.type) {
+    case "naddr":
+    case "nprofile":
+      return result.data.pubkey;
+    case "npub":
+      return result.data;
+    case "nsec":
+      return getPublicKey(result.data);
+  }
+}
---
+++ ui/noogle/src/components/android-signer/types/nostr-event.ts
@@ -0,0 +1,54 @@
+export type ETag = ["e", string] | ["e", string, string] | ["e", string, string, string];
+export type ATag = ["a", string] | ["a", string, string];
+export type PTag = ["p", string] | ["p", string, string] | ["p", string, string, string];
+export type RTag = ["r", string] | ["r", string, string];
+export type DTag = ["d"] | ["d", string];
+export type EmojiTag = ["emoji", string, string];
+export type Tag = string[] | ETag | PTag | RTag | DTag | ATag;
+
+export type NostrEvent = {
+  id: string;
+  pubkey: string;
+  created_at: number;
+  kind: number;
+  tags: Tag[];
+  content: string;
+  sig: string;
+};
+export type CountResponse = {
+  count: number;
+  approximate?: boolean;
+};
+
+export type DraftNostrEvent = Omit<NostrEvent, "pubkey" | "id" | "sig"> & { pubkey?: string; id?: string };
+
+export type RawIncomingEvent = ["EVENT", string, NostrEvent];
+export type RawIncomingNotice = ["NOTICE", string];
+export type RawIncomingCount = ["COUNT", string, CountResponse];
+export type RawIncomingEOSE = ["EOSE", string];
+export type RawIncomingCommandResult = ["OK", string, boolean, string];
+export type RawIncomingNostrEvent =
+  | RawIncomingEvent
+  | RawIncomingNotice
+  | RawIncomingCount
+  | RawIncomingEOSE
+  | RawIncomingCommandResult;
+
+export function isETag(tag: Tag): tag is ETag {
+  return tag[0] === "e" && tag[1] !== undefined;
+}
+export function isPTag(tag: Tag): tag is PTag {
+  return tag[0] === "p" && tag[1] !== undefined;
+}
+export function isRTag(tag: Tag): tag is RTag {
+  return tag[0] === "r" && tag[1] !== undefined;
+}
+export function isDTag(tag: Tag): tag is DTag {
+  return tag[0] === "d";
+}
+export function isATag(tag: Tag): tag is ATag {
+  return tag[0] === "a" && tag[1] !== undefined;
+}
+export function isEmojiTag(tag: Tag): tag is EmojiTag {
+  return tag[0] === "emoji" && tag[1] !== undefined && tag[2] !== undefined;
+}
