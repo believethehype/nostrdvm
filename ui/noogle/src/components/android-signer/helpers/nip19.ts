c277e2d46b884a495e92aeda012b2e2dc06ba8f3
greenart7c3 <greenart7c3@proton.me>
2024-01-31 14:41:15 -0300

    remove some unused code

--- ui/noogle/src/components/android-signer/helpers/nip19.ts
+++ ui/noogle/src/components/android-signer/helpers/nip19.ts
@@ -1,9 +1,5 @@
 import { getPublicKey, nip19 } from "nostr-tools";
 
-export function isHex(str?: string) {
-  if (str?.match(/^[0-9a-f]+$/i)) return true;
-  return false;
-}
 export function isHexKey(key?: string) {
   if (key?.toLowerCase()?.match(/^[0-9a-f]{64}$/)) return true;
   return false;
