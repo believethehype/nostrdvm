6179b767f3e3c0460d20382c93806aede44291af
Believethehype <tobias_baur@yahoo.com>
2024-02-15 10:37:41 +0100

    login with nsec/ncryptsec

--- ui/noogle/src/components/android-signer/helpers/nip19.ts
+++ ui/noogle/src/components/android-signer/helpers/nip19.ts
@@ -1,5 +1,7 @@
 import { getPublicKey, nip19 } from "nostr-tools";
+import { bech32 } from '@scure/base'
 
+export const Bech32MaxSize = 5000
 export function isHexKey(key?: string) {
   if (key?.toLowerCase()?.match(/^[0-9a-f]{64}$/)) return true;
   return false;
@@ -20,4 +22,12 @@ export function getPubkeyFromDecodeResult(result?: nip19.DecodeResult) {
     case "nsec":
       return getPublicKey(result.data);
   }
+}
+
+function encodeBech32<Prefix extends string>(prefix: Prefix, data: Uint8Array): `${Prefix}1${string}` {
+  let words = bech32.toWords(data)
+  return bech32.encode(prefix, words, Bech32MaxSize) as `${Prefix}1${string}`
+}
+export function encodeBytes<Prefix extends string>(prefix: Prefix, bytes: Uint8Array): `${Prefix}1${string}` {
+  return encodeBech32(prefix, bytes)
 }
