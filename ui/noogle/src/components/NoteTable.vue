3195c0c7c27f32fcbfc84796044e1447bc641487
Believethehype <1097224+believethehype@users.noreply.github.com>
2024-03-08 08:27:48 +0100

    rework discovery page

--- ui/noogle/src/components/NoteTable.vue
+++ ui/noogle/src/components/NoteTable.vue
@@ -4,7 +4,7 @@
                  :items="data"
                  :sort-by="sortBy"
                  :sort-type="sortType">
-   <template #item-content="{content, author, authorurl, avatar, indicator, links}">
+   <template #item-content="{content, author, authorurl, avatar, indicator, links, lud16, id, authorid}">
 
    <div class="playeauthor-wrapper">
 
@@ -18,15 +18,22 @@
           {{indicator.time.split("T")[0].split("-")[2].trim()}}.{{indicator.time.split("T")[0].split("-")[1].trim()}}.{{indicator.time.split("T")[0].split("-")[0].trim().slice(2)}}
         </div>
    </div>
+<!--.substr(0, 320) + "\u2026"}} -->
 
-   <p>{{content.substr(0, 320) + "\u2026"}}</p>
+       <h3 v-html="StringUtil.parseImages(content)"></h3>
+   <!--<p>{{content.substr(0, 320) + "\u2026"}}</p> -->
         <div style="padding: 2px; text-align: left;" >
           <a class="menusmall" :href="links.uri" target="_blank">Client</a>
           <a class="menusmall" :href="links.njump" target="_blank">NJump</a>
          <!--<a class="menusmall" :href="links.highlighter" target="_blank">Highlighter</a> -->
           <a class="menusmall":href="links.nostrudel" target="_blank">Nostrudel</a>
-      </div>
 
+
+       <!--<div style="margin-left: auto; margin-right: 10px;" class=" justify-end mt-auto" @click="zap(lud16, id.toHex(), authorid)">
+          <svg style="margin-top:3px" xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="currentColor" class="bi bi-lightning" viewBox="0 0 16 20">
+  <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1z"/></svg></div>-->
+
+      </div>
      </template>
     <!--<template #expand="item">
       <div style="padding: 15px; text-align: left;" >
@@ -51,6 +58,9 @@ import type {Header, Item, SortType} from "vue3-easy-data-table";
 import store from '../store';
 import {types} from "sass";
 import Null = types.Null;
+import StringUtil from "@/components/helper/string";
+import {copyinvoice, createBolt11Lud16, parseandreplacenpubs, zaprequest} from "@/components/helper/Helper.vue";
+import {requestProvider} from "webln";
 defineProps<{
   data?: []
 
@@ -63,6 +73,32 @@ const headers: Header[] = [
   { text: "Results:", value: "content", fixed: true},
  // { text: "Time", value: "indicator.index", sortable: true, },
 ];
+
+
+async function zap(lud16, id, authorid){
+  //let invoice = await zaprequest(lud16, 21 , "with love from noogle.lol", id, authorid, store.state.relays)  //Not working yet
+  if(lud16 != Null && lud16 != ""){
+
+
+    let invoice = await createBolt11Lud16(lud16, 21)
+     let webln;
+    try {
+      webln = await requestProvider();
+    } catch (err) {
+        await copyinvoice(invoice)
+    }
+    if (webln) {
+      try{
+           let response = await webln.sendPayment(invoice)
+
+      }
+      catch(err){
+            console.log(err)
+            await copyinvoice(invoice)
+      }
+    }
+    }
+}
 
 
 </script>
--- ui/noogle/src/components/helper/string.ts
+++ ui/noogle/src/components/helper/string.ts
@@ -1,5 +1,6 @@
-const Regex_Url_Str = "(https:\\/\\/)"+ "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,})+[\\/]{0,1}" // domain name
-
+const Regex_Url_Str = "^https?:\/\/(?:.*\/)?[^\/.]+$" // domain name
+const Regex_Url_Img = "(https?:\\/\\/.*\\.(?:png|jpg|jpeg|webp|gif))" // domain name
+const Regex_Url_Video = "(https?:\\/\\/.*\\.(?:mp4|mov|avi))" // domain name
 
 ;
 
@@ -8,6 +9,10 @@ const Regex_Nip05_Str= "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^
 const StringUtil = {
   isValidUrl: (str: string): boolean => new RegExp(Regex_Url_Str, "i").test(str),
   parseHyperlinks: (str: string): string => str.replace(new RegExp(Regex_Url_Str, "gim"), "<a class='purple' target=\"_blank\" rel=\"noreferrer\" href=\"$&\">$&</a>").replace(new RegExp(Regex_Nip05_Str, "gim"), "<a class='purple' target=\"_blank\" rel=\"noreferrer\" href=\"https://njump.me/$&\">$&</a>"),
+  parseImages: (str: string): string => str.replace(new RegExp(Regex_Url_Img, "gim"), "<img src='$&'/> ").replace(new RegExp(Regex_Url_Video, "gim"), "<video controls muted autoplay src=\"$&\"></video>").replace(new RegExp(Regex_Url_Str, "gim"), "<a class='purple' target=\"_blank\" rel=\"noreferrer\" href=\"$&\">$&</a>"),
+
+
+
 };
 
 export default StringUtil;
