bc5b474a6b8643382ca904777aa3d1d1b36a92e6
Believethehype <1097224+believethehype@users.noreply.github.com>
2024-03-15 16:26:16 +0100

    about nip89 view

--- ui/noogle/src/components/Nip89view.vue
+++ ui/noogle/src/components/Nip89view.vue
@@ -1,4 +1,97 @@
 <template>
+
+    <div class="flex flex-row gap-6 items-center">
+        <Logo />
+        <div class="flex flex-col gap-2">
+            <h1 class="text-7xl font-black tracking-wide">About</h1>
+              <h2 class="text-4xl font-black tracking-wide">Nostr NIP 90 Data Vending Machines</h2>
+          <div class="text-lg text-default">
+                There are many things that make using DVMs a bit of a magical experience.
+            </div>
+        </div>
+    </div>
+<br><br>
+    <div class="grid grid-cols-1 gap-6">
+     <!--    <div class="card card-compact rounded-box bg-black/30">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                    Global improvements
+                </div>
+                <p>
+                    Once a Nostr app supports DVMs that means that they get immediate access to all features,
+                    algorithms, and crazy inventions <em>all</em> DVMs support.</p>
+                <p>
+                    Don't like your client's default <em>Trending</em> algorithm? Pick from hundreds of different
+                    algorithms. Your Highlighter client doesn't work well when trying to read an obscure PDF?
+                    Use a DVM that has better support for it.
+                </p>
+                <p>
+                    You say the spam filtering in your client is too aggressive? or not aggressive enough? Just choose
+                    from a different spam-filtering DVM!
+                </p>
+            </div>
+        </div>
+
+        <div class="card card-compact rounded-box bg-black/30">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                    Long-tail
+                </div>
+                <p>
+                    Because discoverability of these algorithms is solved by the very use of Nostr, we can easily imagine
+                    a future where there are thousands of very specific, very niche and abundantly weird DVMs providing all
+                    kinds of obscure functionalities.
+                </p>
+                <p>
+                    Even if a DVM only had a handful of users, it would still be worth it for its users and for the DVM.
+                </p>
+            </div>
+        </div>
+
+        <div class="card card-compact rounded-box bg-black/30">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                    Reusable results
+                </div>
+                <p>
+                    Every time a Nostr client autotranslates a note, it pings a specific API endpoint to get the
+                    result translated.
+                </p>
+                <p>
+                    Over, and over. The same text being translated by each user.
+                </p>
+                <p>
+                    Since Data Vending Machine results are public by default, once a note has been translated,
+                    all clients can choose to reuse the same translation.
+                </p>
+            </div>
+        </div> -->
+          <div class="card card-compact rounded-box bg-black/30">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                  What is this?
+                </div>
+              <p>Data Vending Machines are data-processing tools on top of th Nostr protocol.
+                </p>
+                 <p>
+                You give them some data, a few sats, and they give you back some data.</p>
+                <p>
+                    This page is just a demo client, showcasing a variety of DVM use-cases. Search Content, Search Profiles, Discover Content, Summarize events, Generate Images, Schedule Notes.
+                    There's an ever growing number of tasks added to the protocol.
+                </p>
+                <p>
+                   These DVMs are not running or being hosted on this site. Instead, the DVMs communicate via Nostr and are available to any App or Client that wants to interact with them.
+                    Want your app or website to support any of these tasks? See NIP90
+                </p>
+                <p>
+                   A List of all DVMs that have a NIP89 announcement is available below, ordered by latest announcement.
+                </p>
+            </div>
+        </div>
+    </div>
+
+<br><br>
+
 <div class="grid gap-6 ">
     <div className="card w-70 bg-base-200 shadow-xl"   v-for="dvm in store.state.nip89dvms"
         :key="dvm.id">
