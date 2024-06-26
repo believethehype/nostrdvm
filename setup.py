fae38514dc502b913b90997175635d3dcc1227e6
Believethehype <1097224+believethehype@users.noreply.github.com>
2024-06-26 13:55:46 +0200

    bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.6.26'
+VERSION = '0.6.27'
 DESCRIPTION = 'A framework to build and run Nostr NIP90 Data Vending Machines'
 LONG_DESCRIPTION = ('A framework to build and run Nostr NIP90 Data Vending Machines. See the github repository for more information')
 
