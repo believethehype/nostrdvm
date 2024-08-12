d8a653484362510f2ae3de7920d7d17add363684
Believethehype <1097224+believethehype@users.noreply.github.com>
2024-08-12 15:55:12 +0200

    update tests

--- nostr_dvm/utils/cashu_utils.py
+++ nostr_dvm/utils/cashu_utils.py
@@ -1,14 +1,53 @@
 import base64
 import json
+
+import httpx
 import requests
+from cashu.core.models import GetInfoResponse, MintMeltMethodSetting
+from cashu.mint.ledger import Ledger
+from cashu.nostr.key import PublicKey, PrivateKey
+from cashu.wallet.wallet import Wallet
+
 from nostr_dvm.utils.database_utils import get_or_add_user
 from nostr_dvm.utils.zap_utils import create_bolt11_ln_bits, create_bolt11_lud16
+
+BASE_URL = "https://mint.minibits.cash/Bitcoin"
+
+
+async def test_create_p2pk_pubkey(wallet1: Wallet):
+    invoice = await wallet1.request_mint(64)
+    # await pay_if_regtest(invoice.bolt11)
+    await wallet1.mint(64, id=invoice.id)
+    pubkey = await wallet1.create_p2pk_pubkey()
+    PublicKey(bytes.fromhex(pubkey), raw=True)
+
+
+async def cashu_wallet():
+    wallet1 = await Wallet.with_db(
+        url=BASE_URL,
+        db="db/Cashu",
+        name="wallet_mint_api",
+    )
+    await wallet1.load_mint()
+    return wallet1
+
+
+async def test_info(ledger: Ledger):
+    response = httpx.get(f"{BASE_URL}/v1/info")
+    assert response.status_code == 200, f"{response.url} {response.status_code}"
+    assert ledger.pubkey
+    assert response.json()["pubkey"] == ledger.pubkey.serialize().hex()
+    info = GetInfoResponse(**response.json())
+    assert info.nuts
+    assert info.nuts[4]["disabled"] is False
+    setting = MintMeltMethodSetting.parse_obj(info.nuts[4]["methods"][0])
+    assert setting.method == "bolt11"
+    assert setting.unit == "sat"
 
 
 async def get_cashu_balance(url):
     from cashu.wallet.wallet import Wallet
     from cashu.core.settings import settings
-
 
     settings.tor = False
     wallet = await Wallet.with_db(
@@ -17,7 +56,7 @@ async def get_cashu_balance(url):
     )
     await wallet.load_mint()
     await wallet.load_proofs()
-    print("Cashu Wallet balance " +  str(wallet.available_balance) + " sats")
+    print("Cashu Wallet balance " + str(wallet.available_balance) + " sats")
     mint_balances = await wallet.balance_per_minturl()
     print(mint_balances)
 
@@ -25,7 +64,6 @@ async def get_cashu_balance(url):
 async def mint_cashu_test(url, amount):
     from cashu.wallet.wallet import Wallet
     from cashu.core.settings import settings
-
 
     settings.tor = False
     wallet = await Wallet.with_db(
@@ -39,7 +77,7 @@ async def mint_cashu_test(url, amount):
     print(mint_balances)
     # mint tokens into wallet, skip if wallet already has funds
 
-    #if wallet.available_balance <= 10:
+    # if wallet.available_balance <= 10:
     #    invoice = await wallet.request_mint(amount)
     #    input(f"Pay this invoice and press any button: {invoice.bolt11}\n")
     #    await wallet.mint(amount, id=invoice.id)
@@ -49,6 +87,7 @@ async def mint_cashu_test(url, amount):
     token_str = await wallet.serialize_proofs(proofs_to_send)
     print(token_str)
     return token_str
+
 
 async def receive_cashu_test(token_str):
     from cashu.wallet.wallet import Wallet
@@ -69,16 +108,11 @@ async def receive_cashu_test(token_str):
 
     print(f"Wallet balance: {wallet.available_balance} sats")
 
-
     try:
         await wallet.redeem(token.token[0].proofs)
         print(f"Wallet balance: {wallet.available_balance} sats")
     except Exception as e:
         print(e)
-
-
-
-
 
 
 def parse_cashu(cashu_token: str):
@@ -98,7 +132,6 @@ def parse_cashu(cashu_token: str):
         total_amount = 0
         for proof in proofs:
             total_amount += proof["amount"]
-
 
         return proofs, mint, total_amount, None
 
@@ -122,7 +155,7 @@ async def redeem_cashu(cashu, config, client, required_amount=0, update_self=Fal
     else:
 
         user = await get_or_add_user(db=config.DB, npub=config.PUBLIC_KEY,
-                               client=client, config=config, update=update_self)
+                                     client=client, config=config, update=update_self)
         invoice = create_bolt11_lud16(user.lud16, estimated_redeem_invoice_amount)
     print(invoice)
     if invoice is None:
@@ -136,7 +169,7 @@ async def redeem_cashu(cashu, config, client, required_amount=0, update_self=Fal
     tree = json.loads(request.text)
     fees = tree["fee"]
     print("Fees on this mint are " + str(fees) + " Sats")
-    redeem_invoice_amount = total_amount -fees
+    redeem_invoice_amount = total_amount - fees
     if redeem_invoice_amount < required_amount:
         err = ("Token value (Payment: " + str(total_amount) + " Sats. Fees: " +
                str(fees) + " Sats) below required amount of  " + str(required_amount)
@@ -149,7 +182,7 @@ async def redeem_cashu(cashu, config, client, required_amount=0, update_self=Fal
     else:
 
         user = await get_or_add_user(db=config.DB, npub=config.PUBLIC_KEY,
-                               client=client, config=config, update=update_self)
+                                     client=client, config=config, update=update_self)
         invoice = create_bolt11_lud16(user.lud16, redeem_invoice_amount)
     print(invoice)
 
