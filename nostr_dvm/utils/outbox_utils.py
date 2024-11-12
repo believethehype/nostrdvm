fd263c881cfcfff4b867f66edeb6bff12a68c6b5
Believethehype <1097224+believethehype@users.noreply.github.com>
2024-11-12 11:10:47 +0100

    Update outbox_utils.py

--- nostr_dvm/utils/outbox_utils.py
+++ nostr_dvm/utils/outbox_utils.py
@@ -53,6 +53,6 @@ AVOID_OUTBOX_RELAY_LIST = ["wss://nos.lol",
                            "wss://paid.spore.ws", "'ws://ofotwjuiv7t6q4azt2fjx3qo7esglmxdeqmh2qvdsdnxw5eqgza24iyd.onion", "wss://r.kojira.io", "wss://nostr-relay.h3z.jp", "wss://relay.yozora.world",
                            "wss://nostr.0xtr.dev", "wss://purplepeg.es", "wss://nostr.mutinywallet.com", "wss://nostr.zebedee.cloud", "wss://relay.wikifreedia.xyz", "wss://relay.exit.pub",
                            "wss://njump.mecheck", "wss://relay.nostr.band", "wss://nostr.kollider.xyz", "wss://fog.dedyn.io", "wss://relay.current.fyi",
-                           "wss://momostr.pink", "wss://nostr.bitcoinlighthouse.de", "wss://140.f7z.io"
+                           "wss://momostr.pink", "wss://nostr.bitcoinlighthouse.de", "wss://140.f7z.io", "wss://relay.nostrcheck.me", "wss://relay.mostr.pub", "wss://purplepag.es", "wss://nostr.bitcoiner.social"
 
                            ]
