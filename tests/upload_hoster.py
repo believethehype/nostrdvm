eb250fd984dac2177a0a28a3ed40fe124704cc91
Believethehype <1097224+believethehype@users.noreply.github.com>
2025-01-02 15:28:31 +0100

    Update upload_hoster.py

--- tests/upload_hoster.py
+++ tests/upload_hoster.py
@@ -1,8 +1,10 @@
 import asyncio
+import os
 from pathlib import Path
 
 import dotenv
-
+from nostr_sdk import Keys
+from nostr_dvm.utils.blossom_utils import upload_blossom
 from nostr_dvm.utils.output_utils import upload_media_to_hoster
 
 if __name__ == '__main__':
@@ -13,5 +15,9 @@ if __name__ == '__main__':
     else:
         raise FileNotFoundError(f'.env file not found at {env_path} ')
 
+    pkey = os.getenv("DVM_PRIVATE_KEY_BOT")
 
-    asyncio.run(upload_media_to_hoster("tests/output.wav", "key", True))
+
+    #asyncio.run(upload_media_to_hoster("tests/image.png", pkey, True))
+    asyncio.run(upload_blossom("tests/cat4.png",  pkey, "https://blossom.primal.net"))
+
