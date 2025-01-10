df6b2b1215164238fd8bd24f0e8268f29c830c41
dbth <1097224+believethehype@users.noreply.github.com>
2025-01-10 21:21:00 +0100

    minor fix, noogle

--- ui/noogle/tailwind.config.js
+++ ui/noogle/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: "class",
@@ -17,6 +19,6 @@ export default {
 
         },
     },
-    plugins: [require("daisyui")],
+     plugins: [daisyui],
 }
 
