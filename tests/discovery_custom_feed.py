5aefcb40e666cff5f7770627871bf06aefd6bf25
dbth <1097224+believethehype@users.noreply.github.com>
2026-09-09 00:32:44 +0200

    remove damus

--- tests/discovery_custom_feed.py
+++ tests/discovery_custom_feed.py
@@ -28,8 +28,7 @@ SYNC_DB_RELAY_LIST = ["wss://relay.ditto.pub",
                       "wss://purplerelay.com",
                       "wss://nostr.bitcoiner.social",
                       "wss://nostr.oxtr.dev",
-                      "wss://relay.nostr.net",
-                      "wss://relay.damus.io"]
+                      "wss://relay.nostr.net"]
 RELAY_LIST = ["wss://relay.primal.net",
               "wss://nostr.mom", "wss://nostr.oxtr.dev",
               "wss://relay.nostr.net"
