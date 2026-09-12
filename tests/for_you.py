e42e6bd83dc39eb691a952a59b3515fd27a98bac
Believethehype <1097224+believethehype@users.noreply.github.com>
2026-09-12 21:14:49 +0200

    Remove flapping relays (nostr.oxtr.dev, relay.nostr.net) from sync lists

--- tests/for_you.py
+++ tests/for_you.py
@@ -29,9 +29,7 @@ RELAY_LIST = ["wss://relay.nostrdvm.com",
 
 SYNC_DB_RELAY_LIST = ["wss://relay.ditto.pub",
                       "wss://purplerelay.com",
-                      "wss://nostr.bitcoiner.social",
-                      "wss://nostr.oxtr.dev",
-                      "wss://relay.nostr.net"]
+                      "wss://nostr.bitcoiner.social"]
 
 if use_logger:
     init_logger(log_level)
